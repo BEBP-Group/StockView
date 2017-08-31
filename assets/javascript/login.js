9ae9b69030e9efafaea3421b65edaa5a27a94f1d
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-31 14:07:52 -0400

    second form

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -7,20 +7,42 @@ var config = {
     messagingSenderId: "797447312452"
   };
  var rootRef;
+ var status = 0;
  var connectedAs = "";
+ var userName = "";
+ var password = "";
+ var whatToDo = "";
  var user = {
  	username: "",
  	password: "",
  	stocks: []
  }
 function process() {
-	var userName = "";
-	var password = "";
-	var whatToDo = "";
 	userName = $("#user-name-input").val().trim();
 	password = $("#password-input").val().trim();
 	whatToDo = $('input[name=joke]:checked').val();
+	if (userName.length == 0) {
+		staus = 1;
+		$("#status").text("user name cannot be null");
+	}
+	if ((status == 0) && (password.length==0)) {
+		status = 1;
+		$("#status").text("password cannot be null");
+	}
 	console.log("in process userName = " + userName);
 	console.log("in process password = " + password);
 	console.log("in process whatToDo " + whatToDo);
+	console.log("whatToDo is " + typeof(whatToDo));
+	if (whatToDo == "create") {
+		$("#panel1").hide();
+		$("#panel2").show();
+	}
+}
+function createPortfolio() {
+	console.log("in createPortfolio");
+
+}
+function initialize() {
+	$("#panel2").hide();
+
 }

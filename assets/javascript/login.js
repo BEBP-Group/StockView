e4ccb3e453184da7f16c8a6ed435c51209951ad0
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-31 09:56:32 -0400

    added radio

---
+++ assets/javascript/login.js
@@ -0,0 +1,8 @@
+function process() {
+	var userName = "";
+	var password = "";
+	userName = $("#user-name-input").val();
+	password = $("#password-input").val();
+	console.log("in process userName = " + userName);
+	console.log("in process password = " + password);
+}

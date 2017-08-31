953d3df1d95d6e4ef3e6d8cb4dc2a87a0afd9f95
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-31 10:33:50 -0400

    more formatting

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,8 +1,8 @@
 function process() {
 	var userName = "";
 	var password = "";
-	userName = $("#user-name-input").val();
-	password = $("#password-input").val();
+	userName = $("#user-name-input").val().trim();
+	password = $("#password-input").val().trim();
 	console.log("in process userName = " + userName);
 	console.log("in process password = " + password);
 }

fe95bbbff29e58f280122479d71f54c8d827ad61
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-31 10:45:18 -0400

    added some firebase

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,6 +1,17 @@
+var config = {
+    apiKey: "AIzaSyDUDwognydTUiE1UKjzsgv7d85dY-4faPE",
+    authDomain: "bob-sproject.firebaseapp.com",
+    databaseURL: "https://bob-sproject.firebaseio.com",
+    projectId: "bob-sproject",
+    storageBucket: "bob-sproject.appspot.com",
+    messagingSenderId: "797447312452"
+  };
+ var rootRef;
+ var connectedAs = "";
 function process() {
 	var userName = "";
 	var password = "";
+	var whatToDo = "";
 	userName = $("#user-name-input").val().trim();
 	password = $("#password-input").val().trim();
 	console.log("in process userName = " + userName);

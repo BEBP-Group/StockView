ed2ec6310eb0973e346d7d9a55dd49dd08d9c528
EdMacall <irvedwmac@gmail.com>
2017-09-09 11:39:43 -0400

    developed dummy page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ var user = {
 };
 */
 
-
+/*
 $("#form1").on("click", function(uname, pwd) {
     this.username = uname;
     this.password  = pwd;
@@ -20,8 +20,34 @@ $("#form1").on("click", function(uname, pwd) {
     console.log("OMGWTF!!!");
     console.log(this);
 });
+*/
+
 
 function Start(userprofile){
   window.open("../../dummy.html");
+  var dummy = "NASDAQ:AAPL:Apple,NYSE:AXP:American Express,NYSE:DD:DuPont,NYSE:WMT:Walmart";
+  var arraySymbols = [];
+  var array = [];
+  var array2 = [];
+  var array3 = [];
+  var stocksId = $("#stocks");
+  var usernameId = $("#username");
+  var passwordId = $("#password");
+  array = dummy.split(",");
+  for(var i = 0; i < array.length; i++)
+  {
+    array2 = array[i].split(":");
+      array3.push(array2[1]);
+  }
 
+  usernameId.text("UserName: ");
+  passwordId.text("PassWord: ");
+  console.log(array3);
+  stocksId.text("Stocks: " + array3[0] + ", " + array3[1] + ", " + array3[2] + ", " + array3[3]);
+  console.log(stocksId);
+
+  // var local = localStorage.getItem("stocks");
+  // console.log(local);
 };
+
+Start();

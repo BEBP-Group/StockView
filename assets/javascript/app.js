fd527993033955853f34dc040ecaa02a1acedd89
EdMacall <irvedwmac@gmail.com>
2017-09-09 12:05:57 -0400

    added images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,13 @@ function Start(userprofile){
   var stocksId = $("#stocks");
   var usernameId = $("#username");
   var passwordId = $("#password");
+  var stockImages = $("#stockImages");
   array = dummy.split(",");
+  var stockSymbols = [["APPL", "assets/images/apple-icon.png"],
+                      ["AXP", "assets/images/amex.png"],
+                      ["DD", "assets/images/dupont.jpeg"],
+                      ["WMT", "assets/images/Walmart_logo_logotype.png"]];
+  console.log(stockSymbols);
   for(var i = 0; i < array.length; i++)
   {
     array2 = array[i].split(":");
@@ -45,6 +51,13 @@ function Start(userprofile){
   console.log(array3);
   stocksId.text("Stocks: " + array3[0] + ", " + array3[1] + ", " + array3[2] + ", " + array3[3]);
   console.log(stocksId);
+
+  for(var i = 0; i < array.length; i++)
+  {
+     var stockImg = $("<img>");
+     stockImg.attr("src", stockSymbols[i][1]);
+     stockImages.append(stockImg);
+  }
 
   // var local = localStorage.getItem("stocks");
   // console.log(local);
